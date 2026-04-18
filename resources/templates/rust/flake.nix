7ccdd13e837c1844c0ca728e1a5ba7ae4456d26f
clayts <clayton.spicer@gmail.com>
2026-04-18 16:23:28 +0100

    Fix

--- resources/templates/rust/flake.nix
+++ resources/templates/rust/flake.nix
@@ -5,7 +5,7 @@
       system = "x86_64-linux";
       pkgs = import inputs.nixpkgs {inherit system;};
     in {
-      default = pkgs.mkShell {
+      "${system}".default = pkgs.mkShell {
         packages = with pkgs; [
           nixd
           alejandra
