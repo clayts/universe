2a54f5ec83b6fbdad1b3b8df31eb6a4a79da0e80
clayts <clayton.spicer@gmail.com>
2026-04-20 17:44:47 +0100

    Cleanup

--- resources/system.sh
+++ resources/system.sh
@@ -3,20 +3,17 @@ set -ueo pipefail
 case "${1:-}" in
   test)
     if [ "$#" -ge 3 ]; then
-        echo "Test $2 at $3"
+        echo "Overriding $2 with path:$3"
         nh os switch /etc/nixos -- --quiet --override-input "$2" "path:$3"
     else
-        echo "Test"
         nh os switch /etc/nixos -- --quiet
     fi
     ;;
   sync)
-    echo "Sync"
    	sudo nix flake update --flake /etc/nixos
     nh os switch /etc/nixos -- --quiet
     ;;
   clean)
-    echo "Clean"
  	nh clean all --optimise
     ;;
   *)
