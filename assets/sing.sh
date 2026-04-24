d0d5a79336e83c66343a4ec554835dc95421df21
clayts <clayton.spicer@gmail.com>
2026-04-24 02:48:09 +0100

    Add emoji

--- assets/sing.sh
+++ assets/sing.sh
@@ -6,4 +6,5 @@ if [[ ! -f "$HOME/.cache/sing/$query.mp3" ]]; then
     mkdir -p "$HOME/.cache/sing"
     yt-dlp -q -t mp3 "ytsearch:$query" -o "$HOME/.cache/sing/$query.mp3"
 fi
+echo "♫"
 mpv --really-quiet --scripts-add="$mpris/share/mpv/scripts/mpris.so" "$HOME/.cache/sing/$query.mp3"
