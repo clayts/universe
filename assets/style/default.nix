efc51ccb7e380028d1e61ae82e0d0ef44c27ab8c
clayts <clayton.spicer@gmail.com>
2026-04-29 23:20:13 +0100

    Refactor

--- assets/style/default.nix
+++ assets/style/default.nix
@@ -1,5 +1,7 @@
-{ pkgs, ... }:
+{ pkgs }:
 {
+  fonts = import ./fonts.nix { inherit pkgs; };
+  colors = builtins.fromJSON (builtins.readFile ./colors.json);
   icons = {
     name = "MoreWaita";
     package = pkgs.morewaita-icon-theme;
@@ -8,40 +10,4 @@
     name = "Bibata-Modern-Classic";
     package = pkgs.bibata-cursors;
   };
-  fonts = {
-    sans = {
-      name = "DeepMind Sans Medium";
-      size = 11;
-      package = pkgs.dm-sans;
-      features = [ ];
-    };
-    serif = {
-      name = "Libre Baskerville";
-      size = 10;
-      package = pkgs.libre-baskerville;
-      features = [ ];
-    };
-    mono = {
-      name = "Maple Mono NF";
-      size = 10;
-      package = pkgs.maple-mono.NF;
-      features = [
-        "calt"
-        "cv02"
-        "cv01"
-        "cv65"
-        "cv66"
-        "ss03"
-        "ss06"
-        "ss11"
-      ];
-    };
-    emoji = {
-      name = "Noto Color Emoji";
-      size = 10;
-      package = pkgs.noto-fonts-color-emoji;
-      features = [ ];
-    };
-  };
-  colors = builtins.fromJSON (builtins.readFile ./colors.json);
 }
---
+++ assets/style/fonts.nix
@@ -0,0 +1,36 @@
+{ pkgs }:
+{
+  sans = {
+    name = "DeepMind Sans Medium";
+    size = 11;
+    package = pkgs.dm-sans;
+    features = [ ];
+  };
+  serif = {
+    name = "Libre Baskerville";
+    size = 10;
+    package = pkgs.libre-baskerville;
+    features = [ ];
+  };
+  mono = {
+    name = "Maple Mono NF";
+    size = 10;
+    package = pkgs.maple-mono.NF;
+    features = [
+      "calt"
+      "cv02"
+      "cv01"
+      "cv65"
+      "cv66"
+      "ss03"
+      "ss06"
+      "ss11"
+    ];
+  };
+  emoji = {
+    name = "Noto Color Emoji";
+    size = 10;
+    package = pkgs.noto-fonts-color-emoji;
+    features = [ ];
+  };
+}
