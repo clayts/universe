1ad03af22dcb9ad6d4fa802e055ef92e47316139
clayts <clayton.spicer@gmail.com>
2026-05-22 05:03:48 +0100

    Add color list comment

--- assets/style/default.nix
+++ assets/style/default.nix
@@ -1,7 +1,25 @@
 { pkgs }:
 {
   fonts = import ./fonts.nix { inherit pkgs; };
-  colors = builtins.fromJSON (builtins.readFile ./colors.json);
+  colors =
+    ## Terminal Color List
+    # black
+    # red
+    # green
+    # yellow
+    # blue
+    # purple
+    # cyan
+    # white
+    # bright-black
+    # bright-red
+    # bright-green
+    # bright-yellow
+    # bright-blue
+    # bright-purple
+    # bright-cyan
+    # bright-white
+    builtins.fromJSON (builtins.readFile ./colors.json);
   icons = {
     name = "MoreWaita";
     package = pkgs.morewaita-icon-theme;
