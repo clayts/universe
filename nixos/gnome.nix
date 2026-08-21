e1b72f65e91f8029924e2b091a0654c4c6b468d9
clayts <clayton.spicer@gmail.com>
2026-08-21 17:48:52 +0100

    Fix proton package name

--- nixos/gnome.nix
+++ nixos/gnome.nix
@@ -28,5 +28,5 @@
     ];
   };
   networking.firewall.checkReversePath = false;
-  environment.systemPackages = with pkgs; [wireguard-tools protonvpn-gui];
+  environment.systemPackages = with pkgs; [wireguard-tools proton-vpn];
 }
