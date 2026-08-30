8270d6c4fc966c8d87aef8d341534f4d36b2abbb
clayts <clayton.spicer@gmail.com>
2026-08-30 13:48:26 +0100

    Add node development flake.nix to templates

---
+++ assets/templates/node/flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  outputs = inputs: {
+    devShells =
+      let
+        system = "x86_64-linux";
+        pkgs = import inputs.nixpkgs { inherit system; };
+      in
+      {
+        "${system}".default = pkgs.mkShell {
+          packages = with pkgs; [
+            nixd
+            alejandra
+
+            nodejs
+            package-version-server
+            superhtml
+          ];
+        };
+      };
+  };
+}
